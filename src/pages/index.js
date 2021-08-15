99b189f263dd7314604e815f215e499f2efecbbc
Oni <oni@Mini.local>
2021-08-15 17:43:47 -0500

    hitcounter

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 import DoorSVG from '../../static/img/entryway.svg';
+import RetroHitCounter from 'react-retro-hit-counter';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -17,6 +18,23 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <p className="hero__subtitle">{siteConfig.customFields.tagline2}</p>
         <p className="hero__subtitle">{siteConfig.customFields.phone}</p>
+        <RetroHitCounter
+          hits={1337}
+          /* The following are all default values: */
+          withBorder={true}
+          withGlow={false}
+          minLength={4}
+          size={40}
+          padding={4}
+          digitSpacing={3}
+          segmentThickness={4}
+          segmentSpacing={0.5}
+          segmentActiveColor="#76FF03"
+          segmentInactiveColor="#315324"
+          backgroundColor="#222222"
+          borderThickness={7}
+          glowStrength={0.5}
+        />
         {/* <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
