d70591224a5058b2f0a16c65f1c08f3fe633f7b0
Oni <oni@Mini.local>
2021-08-16 00:03:29 -0500

    removed retro cntr, added better 1

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/oni/Repositories/github.com/hitechinstall/site/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/oni/Repositories/github.com/hitechinstall/site/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/oni/Repositories/github.com/hitechinstall/site/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/oni/Repositories/github.com/hitechinstall/site/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/oni/Repositories/github.com/hitechinstall/site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/oni/Repositories/github.com/hitechinstall/site/src/css/custom.css"),
-];
